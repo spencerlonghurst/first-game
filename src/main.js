f4f633c17ca4f91a6b6d69841831f9eaa9f5c3bc
Spencer Longhurst <spencer.longhurst@utilisource.us>
2025-04-15 21:29:51 -0600

    Created first game

---
+++ src/main.js
@@ -0,0 +1,38 @@
+import { Start } from './scenes/Start.js';
+
+// const config = {
+//     type: Phaser.AUTO,
+//     title: 'Overlord Rising',
+//     description: '',
+//     parent: 'game-container',
+//     width: 1280,
+//     height: 720,
+//     backgroundColor: '#000000',
+//     pixelArt: false,
+//     scene: [
+//         Start
+//     ],
+//     scale: {
+//         mode: Phaser.Scale.FIT,
+//         autoCenter: Phaser.Scale.CENTER_BOTH
+//     },
+// }
+
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: [
+        Start
+    ],
+};
+
+new Phaser.Game(config);
+            
